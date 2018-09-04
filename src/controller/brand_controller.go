cb23c4f14089e4fe4d8795c59526b4eb3059cd8f
Ryosuke Uchitate <ry0suke.uch1tate@gmail.com>
2018-09-05 00:05:43 +0900

    add get brands

--- src/controller/brand_controller.go
+++ src/controller/brand_controller.go
@@ -9,11 +9,42 @@ import (
 	"strconv"
 	"github.com/b1a9id/go-todo-mvc/src/model"
 		_ "github.com/go-sql-driver/mysql"
+	"time"
 )
 
 var DB *sql.DB
 
 func GetBrands(w http.ResponseWriter, r *http.Request)  {
+	result, err := DB.Query("SELECT * FROM brand ORDER BY id DESC ")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	brands := []model.Brand{}
+	for result.Next() {
+		brand := model.Brand{}
+		var id uint
+		var name string
+		var designer sql.NullString
+		var createdAt, updatedAt time.Time
+
+		err = result.Scan(&id, &name, &designer, &createdAt, &updatedAt)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		brand.ID = id
+		brand.Name = name
+		brand.Designer = designer
+		brand.CreatedAt = createdAt
+		brand.UpdatedAt = updatedAt
+		brands = append(brands, brand)
+	}
+
+	t := template.Must(template.ParseFiles("src/view/list.html"))
+	if err := t.ExecuteTemplate(w, "list.html", brands); err != nil {
+		log.Fatal(err.Error())
+	}
 }
 
 func GetBrand(w http.ResponseWriter, r *http.Request)  {
